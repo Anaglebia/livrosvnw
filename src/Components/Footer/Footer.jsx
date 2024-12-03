13b9bb4a2d9a733d311339ec8c8800f88f917ad4
Anaglebia <bia22.sp@icloud.com>
2024-12-03 13:39:23 -0300

    Finalizei o radape

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,25 +3,28 @@ import Twitter from '../../assets/twitter.png';
 import Youtube from '../../assets/youtube.png';
 import Linkendin from '../../assets/linkendin.png';
 import Instagram from '../../assets/instagram.png';
-import './Footer.scss'
+import S from './footer.module.scss'
 
 function Footer() {
   
 
   return (
-    <footer className="footer">
-     <p>4002-8922</p>
-     <div className='imgens-rodape'>
-      <img className='logo-rodape' src={Facebook} alt="Logo do Facebook" />
-      <img className='logo-rodape' src={Twitter} alt="Logo do Twitter" />
-      <img className='logo-rodape' src={Youtube} alt="Logo do Youtube" />
-      <img className='logo-rodape' src={Linkendin} alt="Logo do Linkendin" />
-      <img className='logo-rodape' src={Instagram} alt="Logo do Instagram" />
-     </div>  
-     <div className='rodape'>
-      <p>Layout desenvolvido por Ana Bezerra - Vai Na Web para fins educativos - 2024  </p>
-     </div>
-    </footer>
+    <footer>
+            <section className={S.boxSocial}>
+                <h3>4002-8922</h3>
+                <nav>
+                    <a href=""> <img src={Facebook} alt="" /> </a>
+                    <a href=""> <img src={Twitter} alt="" /> </a>
+                    <a href=""> <img src={Youtube} alt="" /> </a>
+                    <a href=""> <img src={Linkendin } alt="" /> </a>
+                    <a href=""> <img src={Instagram } alt="" /> </a>
+                </nav>
+            </section>
+            <section className={S.boxEnd}>
+                <p>Layout desenvolvido por Ana Bezerra Vai Na Web para fins educativos - 2024  </p>
+            </section>
+        </footer>
+    
   )
 }
 
